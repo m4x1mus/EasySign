53b25815726317ec5ff911e8bf1b8d3915a5b3a3
PranavPilgaonkar <ppilgaon@in.ibm.com>
2015-01-03 14:15:01 +0530

    Version 0.0.1

--- gen/com/example/osdma/R.java
+++ gen/com/example/osdma/R.java
@@ -34,32 +34,38 @@ public final class R {
         public static final int sync=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
-        public static final int capturedImage=0x7f090000;
-        public static final int deleteBtn=0x7f090002;
-        public static final int image=0x7f090005;
-        public static final int imageGrid=0x7f090003;
-        public static final int itemCheckBox=0x7f090006;
-        public static final int pager=0x7f090004;
-        public static final int selectBtn=0x7f090001;
-        public static final int sync=0x7f090007;
+        public static final int action_settings=0x7f090009;
+        public static final int action_signature=0x7f09000a;
+        public static final int action_verify=0x7f09000b;
+        public static final int btnLogin=0x7f090000;
+        public static final int btnRegister=0x7f090005;
+        public static final int image=0x7f090006;
+        public static final int itemCheckBox=0x7f090007;
+        public static final int pager=0x7f090001;
+        public static final int reg_email=0x7f090003;
+        public static final int reg_fullname=0x7f090002;
+        public static final int reg_password=0x7f090004;
+        public static final int sync=0x7f090008;
     }
     public static final class layout {
         public static final int activity_splash=0x7f030000;
         public static final int capture_layout=0x7f030001;
         public static final int gallery_layout=0x7f030002;
         public static final int main_activity=0x7f030003;
-        public static final int row_grid=0x7f030004;
+        public static final int register_layout=0x7f030004;
+        public static final int row_grid=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
+        public static final int action_signature=0x7f060002;
+        public static final int action_verify=0x7f060003;
         public static final int app_name=0x7f060000;
-        public static final int capture=0x7f060003;
-        public static final int captured_img=0x7f060004;
-        public static final int hello_world=0x7f060002;
+        public static final int capture=0x7f060005;
+        public static final int captured_img=0x7f060006;
+        public static final int hello_world=0x7f060004;
     }
     public static final class style {
         /** 
